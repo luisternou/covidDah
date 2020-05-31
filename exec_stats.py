309ae953664464818fe249ba82a6c25f1f6e7e8b
Andreas Krüger <andreas.krueger@student.tugraz.at>
2020-05-31 12:12:14 +0200

    fixed minor bug

--- exec_stats.py
+++ exec_stats.py
@@ -3,6 +3,18 @@ import os
 from datetime import datetime
 import pandas as pd
 
+source_total_cases = 'covid_stats/sa_covid_stats.0.csv'
+cleaned_total_cases = 'covid_stats/cleaned_up_stats/sa_provinces_confirmed.csv'
+destination_total_cases = 'covid_stats/json_stats/sa_provinces_confirmed.json'
+
+source_closed_cases = 'covid_stats/sa_covid_stats.4.csv'
+cleaned_closed_cases = 'covid_stats/cleaned_up_stats/sa_provinces_closed.csv'
+destination_closed_cases = 'covid_stats/json_stats/sa_provinces_closed.json'
+
+source_deaths_cases = 'covid_stats/sa_covid_stats.6.csv'
+cleaned_deaths_cases = 'covid_stats/cleaned_up_stats/sa_provinces_deaths.csv'
+destination_closed_cases = 'covid_stats/json_stats/sa_provinces_deaths.json'
+
 day_offset = 80
 day_of_year = datetime.now().timetuple().tm_yday
 
@@ -21,39 +33,19 @@ except urllib.error.URLError as e:
     print('URLError: {}'.format(e.reason))
 else:
   command = './html_table_converter -u ' + url + '/' + ' -o covid_stats/sa_covid_stats'
-  print(command)
+  
   os.system(command)
+  csv_content = pd.read_csv(source_total_cases)
+  csv_content.columns = ['province','total_cases','percentage']
+  csv_content.to_csv(cleaned_total_cases)
+  pd.read_csv(cleaned_total_cases).to_json(destination_total_cases)
+  
+  csv_content = pd.read_csv(source_closed_cases)
+  csv_content.columns = ['province','deaths','recoveries']
+  csv_content.to_csv(cleaned_closed_cases)
+  pd.read_csv(cleaned_closed_cases).to_json(destination_closed_cases)
 
-
-# Total cases by province
-
-source_total_cases = 'covid_stats/sa_covid_stats.0.csv'
-cleaned_total_cases = 'covid_stats/cleaned_up_stats/sa_provinces_confirmed.csv'
-destination_total_cases = 'covid_stats/json_stats/sa_provinces_confirmed.json'
-csv_content = pd.read_csv(source_total_cases)
-csv_content.columns = ['province','total_cases','percentage']
-csv_content.to_csv(cleaned_total_cases)
-
-pd.read_csv(cleaned_total_cases).to_json(destination_total_cases)
-
-# closed cases by province
-
-source_closed_cases = 'covid_stats/sa_covid_stats.4.csv'
-cleaned_closed_cases = 'covid_stats/cleaned_up_stats/sa_provinces_closed.csv'
-destination_closed_cases = 'covid_stats/json_stats/sa_provinces_closed.json'
-csv_content = pd.read_csv(source_closed_cases)
-csv_content.columns = ['province','deaths','recoveries']
-csv_content.to_csv(cleaned_closed_cases)
-
-pd.read_csv(cleaned_closed_cases).to_json(destination_closed_cases)
-
-# deaths by age group
-
-source_deaths_cases = 'covid_stats/sa_covid_stats.6.csv'
-cleaned_deaths_cases = 'covid_stats/cleaned_up_stats/sa_provinces_deaths.csv'
-destination_closed_cases = 'covid_stats/json_stats/sa_provinces_deaths.json'
-csv_content = pd.read_csv(source_deaths_cases)
-csv_content.columns = ['age','deaths','percentage']
-csv_content.to_csv(cleaned_deaths_cases)
-
-pd.read_csv(cleaned_deaths_cases).to_json(destination_closed_cases)
+  csv_content = pd.read_csv(source_deaths_cases)
+  csv_content.columns = ['age','deaths','percentage']
+  csv_content.to_csv(cleaned_deaths_cases)
+  pd.read_csv(cleaned_deaths_cases).to_json(destination_closed_cases)

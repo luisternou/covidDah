946e3cf7053edb45c8c3e1a76bf170a73876da59
Andreas Krüger <andreas.krueger@student.tugraz.at>
2020-06-04 08:00:48 +0200

    fixed offset

--- exec_stats.py
+++ exec_stats.py
@@ -15,14 +15,13 @@ source_deaths_cases = 'covid_stats/sa_covid_stats.6.csv'
 cleaned_deaths_cases = 'covid_stats/cleaned_up_stats/sa_provinces_deaths.csv'
 destination_deaths_cases = 'covid_stats/json_stats/sa_provinces_deaths.json'
 
-day_offset = 77
+day_offset = 81
 day_of_year = datetime.now().timetuple().tm_yday
 
 update_number = day_of_year - day_offset
 update_number = str(update_number)
 
 url = 'https://www.nicd.ac.za/covid-19-update-' +update_number
-print(url)
 try:
     conn = urllib.request.urlopen(url)
 except urllib.error.HTTPError as e:

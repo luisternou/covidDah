8c0a2a2f16df96bd1d61b34c290f6aaff9c5b6c7
Andreas Krüger <andreas.krueger@student.tugraz.at>
2020-06-09 08:43:15 +0200

    added district script

---
+++ get_all_districts.sh
@@ -0,0 +1,7 @@
+python3 getDistrictStats.py gp
+python3 getDistrictStats.py ec
+python3 getDistrictStats.py wc
+python3 getDistrictStats.py lp
+python3 getDistrictStats.py mp
+python3 getDistrictStats.py nw
+python3 getDistrictStats.py nc
